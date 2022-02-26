da6f0d9aa6b45dc0abbad162439b9b1c955d0a72
alihassan-here <alihassanuog50@gmail.com>
2022-02-26 10:20:29 +0500

    builtin queue

---
+++ Data Structures/Queue/queue_.js
@@ -0,0 +1,35 @@
+class Queue {
+    constructor() {
+        this.data = [];
+    }
+    enqueue(item) {
+        this.data.push(item);
+    }
+    dequeue() {
+        return this.data.shift();
+    }
+    front() {
+        return this.data[0];
+    }
+    isEmpty() {
+        return this.data.length === 0;
+    }
+    size() {
+        return this.data.length;
+    }
+    print() {
+        console.log(this.data);
+    }
+}
+
+const queue = new Queue();
+
+queue.enqueue(1)
+queue.enqueue(2)
+queue.enqueue(3)
+queue.enqueue(4)
+console.log(queue.dequeue());
+console.log(queue.front());
+console.log(queue.isEmpty());
+queue.print();
+console.log(queue.size());
