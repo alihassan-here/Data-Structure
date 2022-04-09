ef0a9814b65978f92a76d02eb1ae182b9758e983
alihassan-here <alihassanuog50@gmail.com>
2022-04-09 15:47:29 +0500

    steps question

---
+++ Problem Solving/Shapes/Steps Question/Steps(Recursive).js
@@ -0,0 +1,16 @@
+function steps(n, row = 0, stair = '') {
+    // base case
+    if (n === row) return;
+    if (n === stair.length) {
+        console.log(stair);
+        return steps(n, row + 1);
+    };
+    if (stair.length <= row) {
+        stair += "#";
+    } else {
+        stair += " ";
+    }
+    steps(n, row, stair);
+}
+
+steps(3);
