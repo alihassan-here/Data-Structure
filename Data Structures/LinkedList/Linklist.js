5a74d9bef124c840065e2443cee9d30d35714914
alihassan-here <alihassanuog50@gmail.com>
2022-05-01 15:17:58 +0500

    clear, removeFirst, removeLast

--- Data Structures/LinkedList/Linklist.js
+++ Data Structures/LinkedList/Linklist.js
@@ -36,6 +36,28 @@ class LinkedList {
             node = node.next;
         }
     }
+    clear() {
+        this.head = null;
+    }
+    removeFirst() {
+        if (!this.head) return;
+        this.head = this.head.next;
+    }
+
+    removeLast() {
+        if (!this.head) return;
+        if (!this.head.next) {
+            this.head = null;
+            return;
+        }
+        let previous = this.head;
+        let node = this.head.next;
+        while (node.next) {
+            previous = node;
+            node = node.next;
+        }
+        previous.next = null;
+    }
 }
 
 const list = new LinkedList();
